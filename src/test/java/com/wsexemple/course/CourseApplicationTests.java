c4273ac6cafd46fe6466f21e49a63829f8d618a5
eduardolpsss <eduardo.pontes2801@gmail.com>
2024-05-13 11:44:04 -0300

    Project created

---
+++ src/test/java/com/wsexemple/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsexemple.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
